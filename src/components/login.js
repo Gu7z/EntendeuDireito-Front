4928ffd217cdabd3a53ad0673806af67b6a47bc0
Gu7z <gustavoferri13@gmail.com>
2020-01-21 12:48:11 -0400

    OutsideClickHandler adicionado

--- src/components/login.js
+++ src/components/login.js
@@ -1,67 +1,79 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, createRef } from 'react'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export default function Login({ isOverlayed, setOverlay }) {
 
-    function handleSubmit(e){
+
+    function handleSubmit(e) {
         setOverlay(false)
         return true
+    }
+
+    const [isLoginScreen, setLoginScreen] = useState(true)
+
+    const RenderLog = () => {
+        return (
+            <form onSubmit={handleSubmit} >
+                <div class="form-group">
+                    <label htmlFor="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div class="form-group">
+                    <label htmlFor="exampleInputPassword1">Senha</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <br />
+                <button class="btn" style={{ background: '#EA5412', color: 'white', marginBottom: '20px' }}>Entrar</button>
+            </form>
+        )
+    }
+
+    const RenderCad = () => {
+        return (
+            <form onSubmit={handleSubmit} >
+                <div class="form-group">
+                    <label htmlFor="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div class="form-group">
+                    <label htmlFor="exampleInputPassword1">Senha</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                </div>
+                <br />
+                <button class="btn" style={{ background: '#EA5412', color: 'white', marginBottom: '20px' }}>Entrar</button>
+            </form>
+        )
     }
 
     return (
 
         <div>
             {isOverlayed ? (
-                <div style={{
-                    position: "fixed",
-                    width: '100vw',
-                    height: '100vh',
-                    top: 0, left: 0,
-                    bottom: 0,
-                    right: 0,
-                    background: 'rgba(0,0,0,0.5)',
-                    zIndex: 1,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }} onClick={() => {return}} >
-
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        width: 350,
-                        height: 350,
-                        background: 'white',
-                        borderRadius: 15,
-                        color: 'black',
-                    }} >
-                        <div style={{
-                            display: 'flex', 
-                            flexDirection: 'row', 
-                            justifyContent: 'space-around', 
-                            alignItems: 'center',
-                            background: '#d3d3d3', 
-                            height: '100%',
-                            borderTopLeftRadius: 15,
-                            borderTopRightRadius: 15
-                        }} >
-                            <div>Login</div>
-                            <div>Cadastrar</div>
+                <div id="overlay">
+                    <OutsideClickHandler onOutsideClick={()=>{setOverlay(false)}}>
+                        <div id="logCad">
+                            <div id="topLogCad" >
+                                <div id="topLogin" onClick={() => setLoginScreen(!isLoginScreen)}>Login</div>
+                                <div id="topCad" onClick={() => setLoginScreen(!isLoginScreen)}>Cadastrar</div>
+                            </div>
+                            {
+                                isLoginScreen ?
+                                    (
+                                        <>
+                                            <h3> <strong>Logar</strong> </h3>
+                                            <RenderLog></RenderLog>
+                                        </>
+                                    )
+                                    :
+                                    (
+                                        <>
+                                            <h3> <strong>Cadastrar</strong> </h3>
+                                            <RenderCad></RenderCad>
+                                        </>
+                                    )
+                            }
                         </div>
-                        <h3 style={{ paddingTop: '10px' }} > <strong style={{ color: 'black' }} >Logar</strong> </h3>
-                        <form style={{padding: '2% 10%'}} onSubmit={handleSubmit} >
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Email</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Senha</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" />
-                            </div>
-                            <br/>
-                            <button  class="btn" style={{background: '#EA5412', color: 'white', marginBottom: '20px'}}>Entrar</button>
-                        </form>
-                    </div>
-
+                    </OutsideClickHandler>
                 </div>
             ) : null}
         </div>
