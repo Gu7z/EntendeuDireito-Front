ec9b9125d79c13f4bf3081f26e0f36850d9431b7
Gu7z <gustavoferri13@gmail.com>
2020-01-21 22:22:29 -0400

    Arrumando login/cadastrar e adicionando um pouco de validacao

--- src/components/contato.js
+++ src/components/contato.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Contato() {
+
+    const [email, setEmail] = useState('')
+    const [msg, setMsg] = useState('')
+
     return (
         <div className="form_contact">
             <form class="myform" >
                 <div className="form-group">
                     <label htmlFor="exampleFormControlInput1">Seu Email</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                 </div>
                 <div className="form-group" >
                     <label htmlFor="exampleFormControlTextarea1">Sua Mensagem</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="Seja Cordeal!" rows="3"></textarea>
+                    <textarea className="form-control" value={msg} onChange={(e)=>setEmail(e.target.value)} id="exampleFormControlTextarea1" placeholder="Seja Cordeal!" rows="3"></textarea>
                 </div>
                 <button className="btn" style={{background: '#EA5412', borderRadius: 15, marginBottom: 100}}>Enviar!</button>
             </form>
--- src/components/login.js
+++ src/components/login.js
@@ -3,10 +3,29 @@ import OutsideClickHandler from 'react-outside-click-handler';
 
 export default function Login({ isOverlayed, setOverlay }) {
 
+    //Nao posso usar o usestate por causa do overlay aparentemente
+    const form_Values = {
+        email: '', password:''
+    }
+    
+    const change_values = (e) => {
+        form_Values[e.currentTarget.type] = e.target.value
+    }
 
     function handleSubmit(e) {
-        setOverlay(false)
-        return true
+        const isValid = () => {
+            if (form_Values.password.length > 7) {
+                return true
+            } else {
+                return false
+            }
+        }
+        e.preventDefault()
+        if (isValid()){
+            alert('valida')
+        }else{
+            alert('A senha é curta de mais')
+        }
     }
 
     const [isLoginScreen, setLoginScreen] = useState(true)
@@ -16,11 +35,12 @@ export default function Login({ isOverlayed, setOverlay }) {
             <form onSubmit={handleSubmit} >
                 <div class="form-group">
                     <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" onChange={change_values} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div class="form-group">
                     <label htmlFor="exampleInputPassword1">Senha</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <input type="password" onChange={change_values} class="form-control" id="exampleInputPassword1" />
+                    <small>A senha precisa ter no minimo 8 digitos</small>
                 </div>
                 <br />
                 <button class="btn" style={{ background: '#EA5412', color: 'white', marginBottom: '20px' }}>Entrar</button>
@@ -53,8 +73,8 @@ export default function Login({ isOverlayed, setOverlay }) {
                     <OutsideClickHandler onOutsideClick={()=>{setOverlay(false)}}>
                         <div id="logCad">
                             <div id="topLogCad" >
-                                <div id="topLogin" onClick={() => setLoginScreen(!isLoginScreen)}>Login</div>
-                                <div id="topCad" onClick={() => setLoginScreen(!isLoginScreen)}>Cadastrar</div>
+                                <div id="topLogin" onClick={() => setLoginScreen(true)}>Login</div>
+                                <div id="topCad" onClick={() => setLoginScreen(false)}>Cadastrar</div>
                             </div>
                             {
                                 isLoginScreen ?
