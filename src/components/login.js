1411e3c2cc874d2fd351afdcae81176642965fea
Gu7z <gustavoferri13@gmail.com>
2020-01-22 14:36:39 -0400

    Adicionando erros

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import OutsideClickHandler from 'react-outside-click-handler';
 import FadeIn from 'react-fade-in'
+import axios from 'axios'
 
 export default function Login({ isOverlayed, setOverlay }) {
 
@@ -14,18 +15,47 @@ export default function Login({ isOverlayed, setOverlay }) {
     }
 
     function handleSubmit(e) {
-        const isValid = () => {
-            if (form_Values.password.length > 7) {
-                return true
-            } else {
-                return false
-            }
-        }
         e.preventDefault()
-        if (isValid()) {
-            alert('valida')
-        } else {
-            alert('A senha é curta de mais')
+        if(e.target.id == 'formLog'){
+            axios.post('http://localhost:3001/auth', {}, {
+                auth: {
+                    username: form_Values.email,
+                    password: form_Values.password
+                }
+            }).then(
+                response=>console.log(response.data)
+            ).catch(
+                err=>{
+                    console.log(err.response.status)
+                    if(err.response.status === 401){
+                        console.log('Senha incorreta')
+                    }else if(err.response.status === 403){
+                        console.log('Nao confirmou o email')
+                    }else if(err.response.status === 404){
+                        console.log('Nao Cadastrado')
+                    }
+                }
+            )
+        }else{
+            axios.post('http://localhost:3001/create', {}, {
+                auth: {
+                    username: form_Values.email,
+                    password: form_Values.password
+                }
+            }).then(
+                data=>console.log(data)
+            ).catch(
+                err=>{
+                    console.log(err.response.status)
+                    if(err.response.status === 409){
+                        console.log('Usuario ja existe')
+                    }else if(err.response.status === 401){
+                        console.log('Credenciais Invalidas')
+                    }else if(err.response.status === 502){
+                        console.log('Nao Foi possivel enviar o email, entre em contato!')
+                    }
+                }
+            )
         }
     }
 
@@ -33,7 +63,7 @@ export default function Login({ isOverlayed, setOverlay }) {
 
     const RenderLog = () => {
         return (
-            <form onSubmit={handleSubmit} >
+            <form id="formLog" onSubmit={handleSubmit} >
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email</label>
                     <input type="email" onChange={change_values} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
@@ -50,14 +80,14 @@ export default function Login({ isOverlayed, setOverlay }) {
 
     const RenderCad = () => {
         return (
-            <form onSubmit={handleSubmit} >
+            <form id="formCad" onSubmit={handleSubmit} >
                 <div className="form-group">
                     <label htmlFor="exampleInputEmail1">Email</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input type="email" onChange={change_values} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="form-group">
                     <label htmlFor="exampleInputPassword1">Senha</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <input type="password" onChange={change_values} className="form-control" id="exampleInputPassword1" />
                     <small>A senha deve ter no minimo 8 digitos</small>
                 </div>
                 <button className="btn" style={{ background: '#EA5412', color: 'white', marginBottom: '20px' }}>Entrar</button>
